0c07f8370cbb49bd6df6873310d2826b52cc9ef3
Aldo320 <aldoantunez12@gmail.com>
2022-08-30 16:54:22 -0300

    AlexProyect

--- app.js
+++ app.js
@@ -1,32 +1,42 @@
 
-let option = prompt (`Elja Una Opcion:
-1:Spider-Man
-2:The Incredible Hulk
-3:Iron Man
-4:Avengers
+solicitarNombre ()
+function solicitarNombre() {
+    let nombre=prompt ("Ingrese Su Nombre")
+    console.log (`El Nombre De Usuario Es: ${Nombre}`) 
+  }
+
+  function saludar (nombre, apellido){
+    console.log(`Hola ${nombre} ${apellido}`)
+
+let option = prompt (`Elja Tu Marca de Calzado Favorita:
+1:Nike
+2:Adidas
+3:Puma
+4:Reebok
 `)
 
 switch (option) {
     case "1":
-        console.log ("Spider-Man")
+        console.log ("Nike")
 break
     case"2":
-        console.log ("The Incredible Hulk")
+        console.log ("Adidas")
 break
     case "3":
-        console.log ("Iron-Man")
+        console.log ("Puma")
 break    
         case "4":
-        console.log ("Avengers")
+        console.log ("Reebok")
 break
 default:
     console.log ("No Has Ingresado Una Opción Válida")
-}
+} 
 
 let x = 0
 
 while (x <= 10) {
-    console.log (`El Valor de X es $(x)`)
+    console.log (`El Valor de X es ${x}`)
+    x++
 }
 
 let entrada = prompt("Ingrese un texto o ESC para interrumpir")
@@ -45,3 +55,31 @@ let numero = Number(prompt("Ingrese un numero"));
 for (let i = 1; i <= 20; i++) {
   let resultado = numero * i
   alert(`${numero} x ${i} = ${resultado}`);}
+
+
+let primerNumero = 0
+let segundoNumero = 0
+
+function calculadora (primerNumero, segundoNumero, operacion){
+    switch (operacion)
+    {
+        case "+"
+        return primerNumero  segundoNumero
+        break
+        case "-"
+        return primerNumero - segundoNumero
+        break
+        case "*"
+        return primerNumero * segundoNumero;
+        break
+        case "/"
+        return primerNumero / segundoNumero
+        break
+        default
+        return 0
+        break
+    }
+
+    console.log calculadora((20, 30, "+"))
+
+    
